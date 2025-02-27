1cbcdc82222e072133a5efc8d20e21bc09b091e8
Nguyen Van Tien <nguyenvantienthuanan12b3@gmail.com>
2025-02-27 08:56:33 +0700

    upload

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,104 +1,127 @@
-//package core;
-//import com.aventstack.extentreports.ExtentReports;
-//import com.aventstack.extentreports.ExtentTest;
-//import com.aventstack.extentreports.Status;
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import io.qameta.allure.*;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.testng.ITestResult;
-//import org.testng.annotations.*;
-//import pages.LoginPage;
-//
-//import java.time.Duration;
-//
-//public class BaseTest {
-//    protected WebDriver driver;
-//    protected static ExtentReports extent = ExtentReport.getInstance();
-//    protected ExtentTest test;
-//
-//    @BeforeMethod
-//    @Description("Setup browser and launch application")
-//    @Step("Setup Chrome browser")
-//    public void setup() {
-//        try {
-//            String os = System.getProperty("os.name").toLowerCase();
-//            if (os.contains("linux")) {
-//                WebDriverManager.chromedriver().setup();
-//                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-//            } else {
-//                WebDriverManager.chromedriver().setup();
-//            }
-//            driver = new ChromeDriver(getOptions());
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//            driver.manage().window().maximize();
-//
-//            // Tạo test mới trong Extent Reports
-//            test = extent.createTest("Test Case - " + this.getClass().getSimpleName());
-//            test.log(Status.INFO, "Browser setup completed");
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }
-//    }
-//
-//    @AfterMethod
-//    @Description("Close browser and clean up")
-//    public void teardown(ITestResult result) {
-//        if (result.getStatus() == ITestResult.FAILURE) {
-//            test.log(Status.FAIL, "Test Failed: " + result.getThrowable());
-//            test.addScreenCaptureFromPath(takeScreenshotPath());
-//        } else if (result.getStatus() == ITestResult.SUCCESS) {
-//            test.log(Status.PASS, "Test Passed");
-//        }
-//
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//        extent.flush();
-//    }
-//
-//    @AfterSuite
-//    public void closeReport() {
-//        extent.flush();
-//    }
-//
-//    public ChromeOptions getOptions() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
-//        options.addArguments("--disable-extensions", "--password-store=basic", "--disable-popup-blocking");
-//        options.addArguments("--start-maximized", "--disable-web-security", "--allow-running-insecure-content");
-//        options.addArguments("--disable-update", "--user-agent=MyCustomUserAgent");
-//        options.addArguments("--disable-crash-reporter", "--disable-logging");
-//        return options;
-//    }
-//
-//    @Step("Login with username {0} and password {1}")
-//    protected void login(String username, String password) throws InterruptedException {
-//        LoginPage loginPage = new LoginPage(driver);
-//        loginPage.navigateTo("https://www.saucedemo.com/");
-//        loginPage.login(username, password);
-//        takeScreenshot();
-//    }
-//
-//    @Attachment(value = "Screenshot", type = "image/png")
-//    public byte[] takeScreenshot() {
-//        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-//    }
-//
-//    public String takeScreenshotPath() {
-//        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-//    }
-//}
-//
-//
-//
-//
-//
+package core;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import pages.LoginPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected static ExtentReports extent = ExtentReport.getInstance();
+    protected ExtentTest test;
+
+    @BeforeMethod
+    @Description("Setup browser and launch application")
+    @Step("Setup Chrome browser")
+    public void setup() {
+        try {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(getOptions());
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+
+            test = extent.createTest("Test Case - " + this.getClass().getSimpleName());
+            test.log(Status.INFO, "Browser setup completed");
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing WebDriver: " + e.getMessage(), e);
+        }
+    }
+
+    @AfterMethod
+    @Description("Close browser and clean up")
+    public void teardown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "Test Failed: " + result.getThrowable());
+            test.addScreenCaptureFromPath(takeScreenshotPath());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Passed");
+        }
+
+        if (driver != null) {
+            driver.quit();
+        }
+        extent.flush();
+    }
+
+    @AfterSuite
+    public void closeReport() {
+        extent.flush();
+    }
+
+    public ChromeOptions getOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+        options.addArguments("--disable-extensions", "--disable-popup-blocking");
+        options.addArguments("--disable-web-security", "--allow-running-insecure-content");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-software-rasterizer");
+        return options;
+    }
+
+    @Step("Login with username {0} and password {1}")
+    protected void login(String username, String password) throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateTo("https://www.saucedemo.com/");
+        loginPage.login(username, password);
+        takeScreenshot();
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public String takeScreenshotPath() {
+        try {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
+            FileUtils.copyFile(screenshot, new File(path));
+            return path;
+        } catch (IOException e) {
+            System.out.println("Screenshot error: " + e.getMessage());
+            return "";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////Hỗ trợ test song song
 ////
 ////package core;
@@ -198,106 +221,3 @@
 
 
 
-package core;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.*;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.ITestResult;
-import org.testng.annotations.*;
-import pages.LoginPage;
-import java.io.File;
-import java.io.IOException;
-import java.time.Duration;
-
-public class BaseTest {
-    protected WebDriver driver;
-    protected static ExtentReports extent = ExtentReport.getInstance();
-    protected ExtentTest test;
-
-    @BeforeMethod
-    @Description("Setup browser and launch application")
-    @Step("Setup Chrome browser")
-    public void setup() {
-        try {
-            String os = System.getProperty("os.name").toLowerCase();
-            WebDriverManager.chromedriver().setup();
-
-            driver = new ChromeDriver(getOptions(os));
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
-
-            test = extent.createTest("Test Case - " + this.getClass().getSimpleName());
-            test.log(Status.INFO, "Browser setup completed");
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            throw new RuntimeException("WebDriver initialization failed");
-        }
-    }
-
-    @AfterMethod
-    @Description("Close browser and clean up")
-    public void teardown(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            test.log(Status.FAIL, "Test Failed: " + result.getThrowable());
-            test.addScreenCaptureFromPath(takeScreenshotPath());
-        } else if (result.getStatus() == ITestResult.SUCCESS) {
-            test.log(Status.PASS, "Test Passed");
-        }
-
-        if (driver != null) {
-            driver.quit();
-        }
-        extent.flush();
-    }
-
-    @AfterSuite
-    public void closeReport() {
-        extent.flush();
-    }
-
-    public ChromeOptions getOptions(String os) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-popup-blocking", "--disable-extensions");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--start-maximized");
-
-        if (os.contains("linux")) {
-            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-        }
-        return options;
-    }
-
-    @Step("Login with username {0} and password {1}")
-    protected void login(String username, String password) throws InterruptedException {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.navigateTo("https://www.saucedemo.com/");
-        loginPage.login(username, password);
-        takeScreenshot();
-    }
-
-    @Attachment(value = "Screenshot", type = "image/png")
-    public byte[] takeScreenshot() {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-    }
-
-    public String takeScreenshotPath() {
-        try {
-            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            String path = "screenshots/" + System.currentTimeMillis() + ".png";
-            FileUtils.copyFile(screenshot, new File(path));
-            return path;
-        } catch (IOException e) {
-            System.out.println("Screenshot error: " + e.getMessage());
-            return "";
-        }
-    }
-}
